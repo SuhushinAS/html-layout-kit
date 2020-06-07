5443e282d9a6de20626a9916e72ec09f4eb7a038
SuhushinAS <suhushinas@gmail.com>
2020-06-07 18:20:51 +0700

    default component

---
+++ src/modules/example/default/index.js
@@ -0,0 +1,23 @@
+// @flow
+import 'modules/example/default/style.less';
+
+/**
+ * Class ExampleDefault.
+ * Класс для примера.
+ */
+class ExampleDefault {
+    /**
+     * Конструктор класса для примера.
+     * @param {*} exampleDefault Элемент.
+     * @return {void}
+     */
+    constructor(exampleDefault) {
+        this.exampleDefault = exampleDefault;
+    }
+}
+
+const exampleDefaultList = document.querySelectorAll('.j-example-default');
+
+exampleDefaultList.forEach((exampleDefault) => {
+    new ExampleDefault(exampleDefault);
+});
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,4 +1,5 @@
 // @flow
 import 'modules/common/svg-icon';
 import 'modules/layout/layout';
+import 'modules/example/default';
 import './style.less';
