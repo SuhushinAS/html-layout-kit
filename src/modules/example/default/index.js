fb1a2eca346c2e75c39906830567ea956540af16
SuhushinAS <suhushinas@gmail.com>
2020-06-08 09:12:57 +0700

    add custom element

--- src/modules/example/default/index.js
+++ src/modules/example/default/index.js
@@ -1,5 +1,5 @@
 // @flow
-import 'modules/example/default/style.less';
+import './style.less';
 
 /**
  * Class ExampleDefault.
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,4 +2,6 @@
 import 'modules/common/svg-icon';
 import 'modules/layout/layout';
 import 'modules/example/default';
+import 'modules/example/custom';
+import 'modules/example/timer';
 import './style.less';
