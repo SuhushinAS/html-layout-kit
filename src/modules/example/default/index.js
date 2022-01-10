bb1a3f405d2fe4bd3148299e7e2a1242d31fb7ed
SuhushinAS <suhushinas@gmail.com>
2022-01-10 19:32:31 +0700

    fix

--- src/modules/example/default/index.js
+++ src/modules/example/default/index.js
@@ -1,22 +1,16 @@
+import {component} from 'utils/component';
 import './style.less';
 
-/**
- * Class ExampleDefault.
- * Класс для примера.
- */
-class ExampleDefault {
-  /**
-   * Конструктор класса для примера.
-   * @param {*} exampleDefault Элемент.
-   * @return {void}
-   */
-  constructor(exampleDefault) {
-    this.exampleDefault = exampleDefault;
+component(
+  '.example-default',
+  class {
+    /**
+     * Конструктор класса для примера.
+     * @param {*} exampleDefault Элемент.
+     * @return {void}
+     */
+    constructor(exampleDefault) {
+      this.exampleDefault = exampleDefault;
+    }
   }
-}
-
-const exampleDefaultList = document.querySelectorAll('.j-example-default');
-
-exampleDefaultList.forEach((exampleDefault) => {
-  new ExampleDefault(exampleDefault);
-});
+);
